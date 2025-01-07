7f6d58152c62db7986f54d93a4c8a3b47984ab23
Skerrob <skerrobyt@gmail.com>
2025-01-07 22:10:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-/*
-  ptoszek.pl
-  Powered by Jaczup
-
-  My version (ptoszek.pl): 
-  - https://github.com/jaczup/ptoszek.pl
-  Original version (theannoyingsite.com): 
-  - https://github.com/feross/TheAnnoyingSite.com/
-
-  VERIFY THAT YOU ARE DEFINITELY CONTACTING ME! MANY PEOPLE TRY TO IMPERSONATE ME.
-
-  My official social media I use:
-    - YouTube: @jaczup
-    - TikTok: @jaczup_app
-    - Discord: jaczup
-    - Twitter: @jaczup
-    - E-mail: contact@jaczup.me
-
-  Contributors:
-    @jaczup - https://github.com/jaczup
-    @intexpression - https://github.com/intexpression
-    @dan64iel - https://github.com/dan64iel
-    @imzeme - https://github.com/imzeme
-	@GameShoot8050 - https://github.com/GameShoot8050
-    @wetraks -  https://github.com/wetraks
-    @cryblanka - https://github.com/cryblanka
-    @9fm - https://github.com/9fm
-
-*/
-
 const SCREEN_WIDTH = window.screen.availWidth
 const SCREEN_HEIGHT = window.screen.availHeight
 const WIN_WIDTH = 480
@@ -66,11 +36,11 @@ const ART = [
 ]
 
 const SEARCHES = [
-  'jshop',
-  'ptoszek',
-  'ptak',
-  'kanarek',
-  'jaczup'
+  'Koty',
+  'Kociaki',
+  'Kot',
+  'Youtube',
+  'Skerrobek'
 ]
 
 const VIDEOS = [
